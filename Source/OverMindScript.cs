f58bd5d3340d1f295f826cb406d9520afb819b47
TastyTentacles <no email>
2020-11-03 19:03:39 +0000

    basic room mechanic

---
+++ Source/OverMindScript.cs
@@ -0,0 +1,24 @@
+using Godot;
+using Godot.Collections;
+using System;
+
+public class OverMindScript : Node2D {
+    public int cash;
+
+
+    public override void _Ready() {
+        // File f = new File();
+        // f.Open("res://Data/TestData.json", File.ModeFlags.Read);
+
+        // GD.Print(f.GetAsText());
+
+        // Dictionary j = JSON.Parse(f.GetAsText()).Result as Dictionary;
+        // GD.Print(j["name"]);
+
+        // if (d.FileExists("TestData.json")) {
+        //     GD.Print("found it");
+
+
+        // }
+    }
+}
---
+++ Source/RoomScript.cs
@@ -0,0 +1,53 @@
+using Godot;
+using Godot.Collections;
+using System;
+
+public class RoomScript : Node2D {
+    [Export] public string room_type = "basic";
+    [Export] public NodePath dependent_room_path;
+    
+
+    public Label room_name;
+    public RoomScript dependent_room;
+    public ImmediateGeometry line;
+
+    public int slot_count;
+
+    public void LoadRoomData() {
+        File data_raw = new File();
+        data_raw.Open("res://Data/RoomData.json", File.ModeFlags.Read);
+
+        Dictionary data = JSON.Parse(data_raw.GetAsText()).Result as Dictionary;
+        if (data.Contains(room_type)) {
+            Dictionary room_data = data[room_type] as Dictionary;
+
+            room_name.Text = (string) room_data["name"];
+            // slot_count = (int) room_data["slots"];
+        } else {
+            GD.PrintErr("room type not found!");
+        }
+        
+    }
+
+    public override void _Ready() {
+        room_name = GetNode<Label>("Graphics/room_name");
+
+        LoadRoomData();
+
+        if (dependent_room_path != null) {
+            dependent_room = GetNode<RoomScript>(dependent_room_path);
+        }
+    }
+
+    public override void _Draw() {
+        // base._Draw();
+
+        if (dependent_room != null) {
+            Vector2 path = dependent_room.Position - Position;
+            Vector2 path_normal = path.Normalized();
+            float path_distance = path.Length();
+
+            DrawLine(path_normal * (path_distance / 5), path * .80f, new Color(1, 0, 0), 1);
+        }
+    }
+}
