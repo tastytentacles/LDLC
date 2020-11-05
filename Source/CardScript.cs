f0f885a9ef5120396b94f06ddc6daecc92a5f65c
TastyTentacles <no email>
2020-11-05 22:35:21 +0000

    janky but working cards stick to rooms

--- Source/CardScript.cs
+++ Source/CardScript.cs
@@ -1,51 +1,91 @@
 using Godot;
 using System;
 
-public class CardScript : TextureRect {
+using System.Collections.Generic;
+
+public class CardScript : Node2D {
     public bool drag_flag;
     public bool hover_flag;
     public Vector2 offset;
 
     [Export] public Vector2 base_scale = new Vector2(.25f, .25f);
     [Export] public Vector2 hover_scale = new Vector2(.3f, .3f);
-    [Export] public Vector2 place_scale = new Vector2(.1f, .1f);
+    [Export] public Vector2 hold_scale = new Vector2(.1f, .1f);
+    [Export] public Vector2 place_scale = new Vector2(.15f, .15f);
+
+    private List<Area2D> over_list;
+    private Vector2 root_point;
+    private RoomScript over_room;
+
+    private Tween tween = new Tween();
+
 
     public void mouse_in() {
         hover_flag = true;
-        RectScale = hover_scale;
+        Scale = hover_scale;
     }
 
     public void mouse_out() {
         hover_flag = false;
-        RectScale = base_scale;
-
+        Scale = base_scale;
+        GD.Print("mosue out");
     }
 
+
+    public void drop_in(Area2D a) {
+        if (a.GetParent() is RoomScript room && !tween.IsActive()) {
+            GD.Print("over room " + room.Name);
+            Scale = place_scale;
+            // root_point = room.Position;
+
+            over_room = room;
+        }
+    }
+
+    public void drop_out(Area2D a) {
+        if (a.GetParent() == over_room) {
+            Scale = hold_scale;
+            over_room = null;
+        }
+    }
+
+
+
     public override void _Ready() {
-        Connect("mouse_entered", this, nameof(mouse_in));
-        Connect("mouse_exited", this, nameof(mouse_out));
+        AddChild(tween);
+
+        GetNode<Area2D>("hitbox").Connect("mouse_entered", this, nameof(mouse_in));
+        GetNode<Area2D>("hitbox").Connect("mouse_exited", this, nameof(mouse_out));
+
+        GetNode<Area2D>("drop_zone").Connect("area_entered", this, nameof(drop_in));
+        GetNode<Area2D>("drop_zone").Connect("area_exited", this, nameof(drop_out));
     }
 
     public override void _Input(InputEvent e) {
-        if (e.IsPressed() & drag_flag) {
-            GD.Print("trace: " + Name);
-            drag_flag = false;
-            MouseFilter = MouseFilterEnum.Stop;
-            return;
-        }
-        
         if (e is InputEventMouse em) {
-            if (em.IsPressed() & hover_flag) {
-                offset = RectPosition - em.Position;
-                drag_flag = true;
-                MouseFilter = MouseFilterEnum.Ignore;
+            if (em.IsPressed()) {
+                if (over_room != null) {
+                    Scale = place_scale;
+                    root_point = over_room.Position;
+                } else {
+                    Scale = hold_scale;
+                    offset = Position - em.Position;
+                }
+
+                drag_flag = !drag_flag;
             }
         }
     }
 
     public override void _Process(float delta) {
         if (drag_flag) {
-            RectPosition = GetViewport().GetMousePosition() + offset;
+            tween.StopAll();
+
+            Position = GetViewport().GetMousePosition() + offset;
+        } else {
+            tween.InterpolateProperty(this, "position", Position, root_point, .1f);
+            tween.Start();
+            // Position = root_point;
         }
     }
 }
--- Source/OverMindScript.cs
+++ Source/OverMindScript.cs
@@ -7,17 +7,6 @@ public class OverMindScript : Node2D {
 
 
     public override void _Ready() {
-        // File f = new File();
-        // f.Open("res://Data/TestData.json", File.ModeFlags.Read);
 
-        // GD.Print(f.GetAsText());
-
-        // Dictionary j = JSON.Parse(f.GetAsText()).Result as Dictionary;
-        // GD.Print(j["name"]);
-
-        // if (d.FileExists("TestData.json")) {
-        //     GD.Print("found it");
-
-        // }
     }
 }
